57e225d02e6e72aab5f19b3036e1ce94e2d5b404
bwrosen20 <bwrosen20@gmail.com>
2022-12-12 16:05:40 -0500

    commit tree 70b824ea4901b5bafcbd7c7676cc99a98071629e

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(price){
+  if (price<400){
+    let result='This one is on me!';
+    return result;
+  }
+  else if (price>=400 && price<=2000){
+  let result='That will be twenty bucks.';
+  return result;
+  }
+  else if (price>2000 && price<=2500){
+    let result='I will gladly take your thirty bucks.';
+    return result;
+  }
+  else {
+    let result='No can do.'
+    return result;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  if (city=='NYC'){
+    result='Ok, sounds good.'
+    return result;
+  }
+  else {
+    result='No go.'
+    return result;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip=='generous'){
+    let result='Thank you so much.';
+    return result;
+  }
+  else if (tip=='not as generous'){
+    let result='Thank you.';
+    return result;
+  }
+  else {
+    let result='Bye.';
+    return result;
+  }
 }
